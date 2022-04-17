f6d2cfba520a97988e86a55139f1cb12211e63b6
Igor Lesik <ilesik@tachyum.com>
2022-04-16 22:27:37 -0700

    link systemc in minimal test

--- test/minimal.cpp
+++ test/minimal.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
 #include "qpp/qpp.h"
+#include "qsc/qsystemc.h"
 
-int main() {
+int sc_main(int, char**) {
     using namespace qpp;
 
     std::cout << "This is the |0> state:\n";
     std::cout << disp(0_ket) << '\n';
+
+    cmat U = gt.X;
+    ket result = U * 0_ket;
+
+    std::cout << ">> The result of applying the bit-flip gate X on |0> is:\n";
+    std::cout << disp(result) << '\n';
+
+    return 0;
 }
